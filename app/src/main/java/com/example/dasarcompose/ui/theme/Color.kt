f09068d63b981874d85c9670947fc007cfdf73ee
Venesseus <satriayusufrafaly@gmail.com>
2023-06-08 02:26:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/dasarcompose/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.dasarcompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val LightBlue = Color(0xFFD7EFFE)
+val Navy = Color(0xFF073042)
+val Chartreuse = Color(0xFFEFF7CF)
